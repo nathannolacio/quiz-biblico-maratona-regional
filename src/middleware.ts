8624602fcb041bcf317f24e63e8c69971683b311
nathannolacio <nathannolacio04@gmail.com>
2026-06-16 20:44:31 -0300

    :wrench: fix: Ajustando middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
 
 export function middleware(req: NextRequest) {
+  console.log("MIDDLEWARE:", req.nextUrl.pathname);
   const token = req.cookies.get("auth_token")?.value;
+  const path = req.nextUrl.pathname;
 
-  const isLoginPage = req.nextUrl.pathname === "/login";
+  const isLoginPage = path.startsWith("/login");
 
   const isProtectedRoute =
-    req.nextUrl.pathname.startsWith("/quiz") ||
-    req.nextUrl.pathname.startsWith("/result") ||
-    req.nextUrl.pathname.startsWith("/chapters");
+    path.startsWith("/quiz") ||
+    path.startsWith("/result") ||
+    path.startsWith("/chapters");
 
   if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", req.url));
